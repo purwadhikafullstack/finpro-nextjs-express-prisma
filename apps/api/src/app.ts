314420d640a3065ff598832f3d987ddfe06374e2
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-04-04 03:01:53 +0700

    chore(sample): :fire: remove default code from fork

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -9,7 +9,6 @@ import express, {
 } from 'express';
 import cors from 'cors';
 import { PORT } from './config';
-import { SampleRouter } from './routers/sample.router';
 
 export default class App {
   private app: Express;
@@ -51,13 +50,9 @@ export default class App {
   }
 
   private routes(): void {
-    const sampleRouter = new SampleRouter();
-
     this.app.get('/', (req: Request, res: Response) => {
       res.send(`Hello, Purwadhika Student !`);
     });
-
-    this.app.use('/samples', sampleRouter.getRouter());
   }
 
   public start(): void {
